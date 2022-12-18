43ad4e7af438a749220f23a316af2b407aa22e4b
Ray <851836818@qq.com>
2022-12-18 13:00:37 +0800

    Add files via upload

--- back-end/app/api/load_json.py
+++ back-end/app/api/load_json.py
@@ -5,7 +5,7 @@ import re
 import json
 
 @bp.route('/load_json',methods=["GET","POST"])
-def remove_json():
+def load_json():
 	#nxt_json_dir='/mnt/d/My_Python_Web_development/StrainDetect_Develop/Tree_test/data/'
 	data=request.form
 	jid=data['jid']
